046f3737b36fb1c8a88a7ec64f1b9e58a0bc8b34
ottermoa <73781001+ottermoa@users.noreply.github.com>
2021-01-25 19:31:50 +0700

    Calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+let numberInput = document.getElementById("number-Input");
+let runButton = document.getElementById("run-button");
+let output = document.getElementById("output");
+
+function printMultiply() {
+    let number = Number(numberInput.value); //ดึงค่า
+    let outputHtml = "";
+
+    if (number === 0); {
+        output.innerHTML = "มุแง";
+    }
+
+    for (let i = 1; i <= 12; i++) {
+        outputHtml += "<p>";
+        outputHtml += number + " x " + i + " = " + ( number * i );
+        outputHtml += "</p>";
+        //return; //เมื่อ code เจอคำสั่งนี้ จะไม่ทำข้างล่างต่อ
+    }
+
+    output.innerHTML = outputHtml;
+}
+
+runButton.addEventListener("click", printMultiply);
