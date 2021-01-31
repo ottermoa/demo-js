640a9ff36af9204ca2d793c932b3d2d7d2d9d1b2
ottermoa <73781001+ottermoa@users.noreply.github.com>
2021-01-31 20:02:21 +0700

    Array

---
+++ array.js
@@ -0,0 +1,26 @@
+//array functions
+
+//normal function
+function saySomething() {
+    console.log("สวัสดีจ้า")
+}
+
+//Array Functions
+
+let sayButton = document.getElementById('say-Button');
+
+//function saySomething1() {
+//    console.log("มีอะไรค๊า");
+//}
+// ^---- แบบเดิม
+function showMessage() {
+    message.innerHTML = "kuay";
+}
+sayButton.addEventListener('click', function() {
+    console.log('มีอะไรค๊าพส');
+});
+
+// ประหยัดบรรทัด ไม่ต้องใช้function ซ้ำ
+
+
+
