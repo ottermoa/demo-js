5ec31b0f58af724983a2d7234bbf299590cae1c0
ottermoa <73781001+ottermoa@users.noreply.github.com>
2021-02-04 20:16:42 +0700

    Arrow

---
+++ arrow.js
@@ -0,0 +1,26 @@
+//arrow functions
+
+//normal function
+function saySomething() {
+    console.log("สวัสดีจ้า")
+}
+
+//Arrow Functions
+
+let sayButton = document.getElementById('say-Button');
+
+//function saySomething1() {
+//    console.log("มีอะไรค๊า");
+//}
+// ^---- แบบเดิม
+function showMessage() {
+    message.innerHTML = "kuay";
+}
+sayButton.addEventListener('click', function() {
+    console.log('มีอะไรค๊าพส');
+});
+
+// ประหยัดบรรทัด ไม่ต้องใช้function ซ้ำ
+
+
+
---
+++ arrow1.js
@@ -0,0 +1,18 @@
+let sayButton = document.getElementById("say-button");
+let Wonder = document.getElementById("Wonder");
+
+
+
+sayButton.addEventListener('click', function() {
+    Wonder.innerHTML = "where are my csl";
+    console.log('Madailaew');
+    
+});
+
+
+//sayButton.addEventListener('click', showMessage);
+
+
+
+
+
